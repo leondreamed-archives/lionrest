c3981c9c302786b81afbfa5b2526a9154da40f0a
Leon Si <contact@leonzalion.com>
2021-12-07 16:45:23 -0500

    export some types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+export type {
+	BaseGetSchema,
+	BaseNonGetSchema,
+	BaseRouteMethodSchema,
+	BaseRouteSchema,
+} from './types/schema';
 export { createLionrest } from './utils/lionrest';
 export { defNullReply, defReply } from './utils/reply';
 export { defRestSchema } from './utils/schema';
