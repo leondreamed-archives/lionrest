b33e223cb9d2696f21d0909db25da7196d0e603e
Leon Si <contact@leonzalion.com>
2021-12-08 00:00:17 -0500

    types

--- src/modules/reply/reply.ts
+++ src/modules/reply/reply.ts
@@ -9,29 +9,46 @@ export function replyModule<B extends RestSchemaBlueprint>() {
 	const defineMethods = useDefineMethods<B>();
 
 	return defineMethods({
-		useReplyCreator: function <
+		useRouteCreator: function <
 			Method extends HttpMethod,
 			Url extends RestSchemaUrls<MethodRoutes<B, Method>>
 		>(method: Method, url: Url): ServerRepliesCreator<B, Url, Method> {
-			const serverReplies = {} as ServerRepliesCreator<B, Url, Method>;
+			const routeCreator = {} as ServerRepliesCreator<B, Url, Method>;
 			for (const [code, { data, statusCode }] of Object.entries(
 				this.schemaBlueprint[url]![method]!.replies
 			)) {
 				if (data === null) {
-					(serverReplies as any)[code] = () => ({
+					(routeCreator as any)[code] = () => ({
 						statusCode,
 						code,
 						data: null,
 					});
 				} else {
-					(serverReplies as any)[code] = (data: unknown) => ({
+					(routeCreator as any)[code] = (data: unknown) => ({
 						statusCode,
 						code,
 						data,
 					});
 				}
 			}
-			return serverReplies;
+
+			return routeCreator;
+		},
+		fastifyRoute: function <
+			Method extends HttpMethod,
+			Url extends RestSchemaUrls<MethodRoutes<B, Method>>
+		>(
+			method: Method,
+			url: Url
+		): ServerRepliesCreator<B, Url, Method> & {
+			method: Uppercase<HttpMethod>;
+			url: Url;
+		} {
+			const routeCreator = this.useRouteCreator(method, url);
+			return Object.assign(routeCreator, {
+				method: method.toUpperCase() as Uppercase<HttpMethod>,
+				url,
+			});
 		},
 	});
 }
