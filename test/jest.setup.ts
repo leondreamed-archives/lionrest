dddb28f4f5988b6907bbc53f08fbf0e1ce1d81d7
Leon Si <contact@leonzalion.com>
2021-12-07 20:04:22 -0500

    add comment

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,5 +1,6 @@
 import fetch, { Headers, Request } from 'node-fetch';
 
+// Stuff needed for ky to work
 (globalThis.fetch as any) = fetch;
 (globalThis.Request as any) = Request;
 globalThis.Headers = Headers;
